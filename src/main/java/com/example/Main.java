289fcd5fb043fe51e1516137f30d0306fb01b75a
dipaktewari67 <dipaktewari67@gmail.com>
2018-12-21 14:06:38 +0530

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,88 +1,81 @@
 /*
- * Copyright 2002-2014 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
+package com;
 
-package com.example;
-
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Map;
-
-@Controller
-@SpringBootApplication
-public class Main {
-
-  @Value("${spring.datasource.url}")
-  private String dbUrl;
-
-  @Autowired
-  private DataSource dataSource;
-
-  public static void main(String[] args) throws Exception {
-    SpringApplication.run(Main.class, args);
-  }
-
-  @RequestMapping("/")
-  String index() {
-    return "index";
-  }
-
-  @RequestMapping("/db")
-  String db(Map<String, Object> model) {
-    try (Connection connection = dataSource.getConnection()) {
-      Statement stmt = connection.createStatement();
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
-      ArrayList<String> output = new ArrayList<String>();
-      while (rs.next()) {
-        output.add("Read from DB: " + rs.getTimestamp("tick"));
-      }
-
-      model.put("records", output);
-      return "db";
-    } catch (Exception e) {
-      model.put("message", e.getMessage());
-      return "error";
+    import java.sql.Connection;
+    import java.sql.DriverManager;
+    import java.sql.ResultSet;
+    import java.sql.SQLException;
+    import java.sql.Statement;
+    import java.sql.Timestamp;
+    import java.util.Timer;
+/**
+*
+* @author pandhir
+*/
+public class App {
+    
+    private final String url = "jdbc:postgresql://ec2-23-21-188-236.compute-1.amazonaws.com:5432/df65a718i89dtb";
+    private final String user = "xyjjlzjifvyfzq";
+    private final String password = "60a0aafc09296d38da99e3a72c58782d47e5c0c4af4bc5632b2a9a167ce1dedc";
+     
+    private static Timestamp starttime;
+    private static Timestamp endtime;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        App app = new App();
+        app.connect();
+        
+        
     }
-  }
-
-  @Bean
-  public DataSource dataSource() throws SQLException {
-    if (dbUrl == null || dbUrl.isEmpty()) {
-      return new HikariDataSource();
-    } else {
-      HikariConfig config = new HikariConfig();
-      config.setJdbcUrl(dbUrl);
-      return new HikariDataSource(config);
+    
+    public Connection connect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+            Statement stmt = conn.createStatement();
+            System.out.println("after connection");
+            ResultSet rs = stmt.executeQuery("select \"Voltage\" from public.\"Voltage Table\" limit 10");
+            System.out.println("hello");
+            while (rs.next()){
+            	System.out.println(rs.getString(1));
+            	//String voltage = rs.getString("Voltage");
+            	//System.out.format("%s\n",voltage);
+            }
+           /* if(rs.next()){
+                if(starttime != null){
+                   
+                }
+                else{
+                    starttime = rs.getTimestamp("event_datetime__c");
+                    long noofmillisecs = starttime.getTime()+15*60*1000;
+                    endtime = new Timestamp(noofmillisecs);
+                }
+                 App app = new App();
+                 app.timerfunction(starttime,endtime);
+                 
+            } **/
+           
+        } catch (SQLException e) {
+            System.out.println("here 11 "+e.getMessage());
+        }
+        return conn;
     }
-  }
-
+    
+    public void timerfunction(Timestamp startTime,Timestamp endTime) throws SQLException {
+        
+        //new TimerClass(startTime,endTime).callgcp();
+        
+        Timer timer = new Timer();
+        timer.schedule(new TimerClass(startTime,endTime), 0, 5000);
+    }
+    
 }
+
